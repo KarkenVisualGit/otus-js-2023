b6fd319f6963904e3d7b8f467228df46097aa131
Karken Turarov <karken_turarov@mail.ru>
2023-08-14 15:01:43 +0600

    edit suof3digit test

--- src/sumof3digits.js
+++ src/sumof3digits.js
@@ -1,12 +1,9 @@
-function sumof3digitNumber(num) {
+function sumof3digitNumber() {
+    const num = prompt('Enter the number: ');
     const digit3 = num % 10;
     const digit1 = (num - num % 100) / 100;
     const digit2 = (num - digit1 * 100 - digit3) / 10;
     return digit1 + digit2 + digit3;
 }
-
-const num = prompt('Enter the number: ');
-
-console.log(sumof3digitNumber(num));
 
 module.exports = sumof3digitNumber
