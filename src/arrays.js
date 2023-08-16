a8df81f5885b83ff48ed0ffc317c4208888217e4
Karken Turarov <karken_turarov@mail.ru>
2023-08-17 01:18:34 +0600

    edit arrays

--- src/arrays.js
+++ src/arrays.js
@@ -4,12 +4,20 @@ for (let i = 0; i < 10; i++)
 {
     // let num = prompt('Enter a value of array');
     // let n = Number(num);
-    nums.push(Math.random());
+    nums.push(Math.ceil(Math.random(0.5)*7));
 }
-console.log(nums);
+// console.log(nums.sort((a,b) => a - b));
 const newnums = nums.forEach(element => 
     {
         sum+=element;
     }
 )
 console.log(sum);
+const array = nums.map(x => x * 2);
+// console.log(array);
+// console.log(array.sort((a,b) => a - b));
+orderedArray = nums.sort((a,b) => a - b);
+let min = orderedArray[0];
+let max = orderedArray[9];
+console.log(max);
+console.log(min);
