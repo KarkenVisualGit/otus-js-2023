9a6d90d15b61e008b7144b9f56498ba6a7b9bc19
Karken Turarov <karken_turarov@mail.ru>
2023-08-26 21:01:16 +0600

    edit sumof3Digits tests

--- _tests_/sumof3digits.test.js
+++ _tests_/sumof3digits.test.js
@@ -3,14 +3,35 @@ const sumof3digitNumber = require('../src/sumof3digits');
 describe("sum of digits in 3-digit number", () => {
   it("returns 18 for 567", () => {
     jest.spyOn(window, "prompt").mockImplementation(() => "567");
-    expect(sumof3digitNumber()).toBe(18);
+    const logSpy = jest.spyOn(global.console, 'log');
+    sumof3digitNumber();
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith(18);
+
+    logSpy.mockRestore();
   });
   it("returns 6 for 123", () => {
     jest.spyOn(window, "prompt").mockImplementation(() => "123");
-    expect(sumof3digitNumber()).toBe(6);
+    const logSpy = jest.spyOn(global.console, 'log');
+    sumof3digitNumber();
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith(6);
+
+    logSpy.mockRestore();
   });
   it("returns 14 for 257", () => {
     jest.spyOn(window, "prompt").mockImplementation(() => "257");
-    expect(sumof3digitNumber()).toBe(14);
+    const logSpy = jest.spyOn(global.console, 'log');
+    sumof3digitNumber();
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith(14);
+
+    logSpy.mockRestore();
   });
 });
--- src/sumof3digits.js
+++ src/sumof3digits.js
@@ -3,7 +3,7 @@ function sumof3digitNumber() {
     const digit3 = num % 10;
     const digit1 = (num - num % 100) / 100;
     const digit2 = (num - digit1 * 100 - digit3) / 10;
-    return digit1 + digit2 + digit3;
+    console.log(digit1 + digit2 + digit3);
 }
 
 module.exports = sumof3digitNumber
