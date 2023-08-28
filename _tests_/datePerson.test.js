4f1d919dd1a0f9580eab42d660387ce4b5770bd2
Karken Turarov <karken_turarov@mail.ru>
2023-08-28 18:07:21 +0600

    add datePerson test

---
+++ _tests_/datePerson.test.js
@@ -0,0 +1,18 @@
+import { getOlder } from "../src/datePersons";
+
+describe('Test getOlder', () => {
+    
+    it("Should return older person", () => {
+        jest.spyOn(window, "prompt").mockImplementation(() => "01.01.1979");
+        jest.spyOn(window, "prompt").mockImplementation(() => "15.06.1972");
+        
+        const logSpy = jest.spyOn(global.console, 'log');
+        getOlder();
+
+        expect(logSpy).toHaveBeenCalled();
+        expect(logSpy).toHaveBeenCalledTimes(1);
+        expect(logSpy).toHaveBeenCalledWith('First person is older');
+
+        logSpy.mockRestore();
+    });
+});
--- src/datePersons.js
+++ src/datePersons.js
@@ -1,20 +1,24 @@
-let persondate1 = prompt('DD.MM.YYYY');
-const [dd, mm, yr] = persondate1.split('.');
-const personDate1 = new Date(`${yr}-${mm}-${dd}`);
-let persondate2 = prompt('DD.MM.YYYY');
-const [dd1, mm1, yr1] = persondate2.split('.');
-const personDate2 = new Date(`${yr1}-${mm1}-${dd1}`);
+export function getOlder() {
+    let persondate1 = prompt('DD.MM.YYYY');
+    const [dd, mm, yr] = persondate1.split('.');
+    const personDate1 = new Date(`${yr}-${mm}-${dd}`);
+    let persondate2 = prompt('DD.MM.YYYY');
+    const [dd1, mm1, yr1] = persondate2.split('.');
+    const personDate2 = new Date(`${yr1}-${mm1}-${dd1}`);
 
-const person1 = {
-    date: personDate1,
-    name: 'John first'
-};
-const person2 = {
-    date: personDate2,
-    name: 'Sam second'
-}
-let diffDate = personDate1 - personDate2;
-diffDate = diffDate / 1000 / 60 / 60 / 24;
-function older() {
-    return diffDate < 0 ? person1.name : person2.name;
+    const person1 = {
+        date: personDate1,
+        name: 'First person'
+    };
+    const person2 = {
+        date: personDate2,
+        name: 'Second person'
+    }
+    let diffDate = personDate1 - personDate2;
+    diffDate = diffDate / 1000 / 60 / 60 / 24;
+    function older() {
+        return diffDate > 0 ? person2.name + " is older":
+        person1.name + " is older";
+    }
+    console.log(older());
 }
