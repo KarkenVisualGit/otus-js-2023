443ef5387fab3d78140e8ab3c36f9b2cee9e62a7
Karken Turarov <karken_turarov@mail.ru>
2023-08-30 22:25:56 +0600

    add circle.js and circle test

---
+++ src/circle.js
@@ -0,0 +1,13 @@
+export function calculateCircleProperties(r) {
+    const circleLength = 2 * Math.PI * r;
+    const circleArea = Math.PI * Math.pow(r, 2);
+
+    console.log(Math.floor(circleLength));
+    console.log(Math.floor(circleArea));
+
+    return {
+      circleLength: Math.floor(circleLength),
+      circleArea: Math.floor(circleArea)
+    };
+    
+  }
