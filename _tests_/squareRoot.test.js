1c192dd6872532ac8ca371ff64f4fc27cd29157a
Karken Turarov <karken_turarov@mail.ru>
2023-09-01 11:12:44 +0600

    edit squareRoot squareRoot test

--- _tests_/squareRoot.test.js
+++ _tests_/squareRoot.test.js
@@ -1,51 +1,46 @@
 import { calculateSquareRoot } from '../src/squareRoot';
+describe('Test calculateSquareRoot', () => {
+  jest.spyOn(window, 'prompt')
+  .mockReturnValueOnce('1')
+  .mockReturnValueOnce('3')
+  .mockReturnValueOnce('-4');
 
-describe('calculateSquareRoot', () => {
-  it('returns correct roots for quadratic equation with two real roots', () => {
-    jest.spyOn(window, "prompt").mockImplementation(() => "1");
-    jest.spyOn(window, "prompt").mockImplementation(() => "-3");
-    jest.spyOn(window, "prompt").mockImplementation(() => "2");
-    const logSpy = jest.spyOn(global.console, 'log');
-    // const a = 1;
-    // const b = -3;
-    // const c = 2;
-    
-    // const result = calculateSquareRoot(a, b, c);
-    calculateSquareRoot();
-    expect(logSpy).toHaveBeenCalled();
-    expect(logSpy).toHaveBeenCalledTimes(1);
-    expect(logSpy).toHaveBeenCalledWith({ roots: [2, 1], message: '' });
+const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
 
-    expect(result.roots).toEqual([2, 1]);
-    expect(result.message).toBe('');
-  });
+test('calculateSquareRoot calculates and logs correct roots', () => {
+  calculateSquareRoot();
 
-  it('returns correct root for quadratic equation with one real root', () => {
-    jest.spyOn(window, "prompt").mockImplementation(() => "1");
-    jest.spyOn(window, "prompt").mockImplementation(() => "-2");
-    jest.spyOn(window, "prompt").mockImplementation(() => "1");
-    const logSpy = jest.spyOn(global.console, 'log');
-    // const a = 1;
-    // const b = -2;
-    // const c = 1;
+  expect(consoleLogSpy).toHaveBeenCalledWith('X1= 1', 'X2 = -4');
+});
 
-    // const result = calculateSquareRoot(a, b, c);
-    calculateSquareRoot();
-    expect(logSpy).toHaveBeenCalled();
-    expect(logSpy).toHaveBeenCalledTimes(1);
-    expect(logSpy).toHaveBeenCalledWith({ roots: [1], message: '' });
-    expect(result.roots).toEqual([1]);
-    expect(result.message).toBe('');
-  });
+test('calculateSquareRoot logs "The equation has no roots" for negative discriminant', () => {
+  jest.clearAllMocks();
+  jest.spyOn(window, 'prompt')
+    .mockReturnValueOnce('1')
+    .mockReturnValueOnce('2')
+    .mockReturnValueOnce('3');
 
-  it('returns no roots message for quadratic equation with no real roots', () => {
-    const a = 1;
-    const b = 1;
-    const c = 1;
+  calculateSquareRoot();
 
-    const result = calculateSquareRoot(a, b, c);
+  expect(consoleLogSpy).toHaveBeenCalledWith('The equation has no roots');
+});
 
-    expect(result.roots).toEqual([]);
-    expect(result.message).toBe('The equation has no roots');
-  });
+test('calculateSquareRoot calculates and logs one root', () => {
+  jest.clearAllMocks();
+  jest.spyOn(window, 'prompt')
+    .mockReturnValueOnce('1')
+    .mockReturnValueOnce('-2')
+    .mockReturnValueOnce('1');
+
+  calculateSquareRoot();
+
+  expect(consoleLogSpy).toHaveBeenCalledWith('X = 1');
+});
+
+// Restore original implementations after tests
+afterAll(() => {
+  window.prompt.mockRestore();
+  console.log.mockRestore();
+});
+  
 });
--- src/squareRoot.js
+++ src/squareRoot.js
@@ -1,25 +1,19 @@
 
-function calculateSquareRoot() {
-  // let a = Number(prompt('Enter a coefficient'));
-  // let b = Number(prompt('Enter b coefficient'));
-  // let c = Number(prompt('Enter c coefficient'));
-    const a = 1;
-    const b = -3;
-    const c = 2;
-  const discr = Math.pow(b, 2) - 4 * a * c;
-  const result = { roots: [], message: '' };
-
+export function calculateSquareRoot() {
+  let a = Number(prompt('Enter a coefficient'));
+  let b = Number(prompt('Enter b coefficient'));
+  let c = Number(prompt('Enter c coefficient'));
+  let x = 0;
+  const discr = Math.pow(b, 2) - (4 * a * c);
   if (discr < 0) {
-    result.message = 'The equation has no roots';
+      console.log('The equation has no roots');
   } else if (discr === 0) {
-    const x = (-b) / (2 * a);
-    result.roots.push(x);
+      x = (-b) / (2 * a);
+      console.log("X = " + x);
   } else if (discr > 0) {
-    const x1 = (-b + Math.sqrt(discr)) / (2 * a);
-    const x2 = (-b - Math.sqrt(discr)) / (2 * a);
-    result.roots.push(x1, x2);
+      let x1 = ((-b) + Math.sqrt(discr)) / 2 * a;
+      let x2 = ((-b) - Math.sqrt(discr)) / 2 * a;
+      console.log("X1= " + x1, "X2 = " + x2);
   }
-  console.log(result);
-  return result;
 }
-calculateSquareRoot();
+// calculateSquareRoot();
