ef4e1c5a75c20d43240fe57b8e6a600b4c42a9df
Karken Turarov <karken_turarov@mail.ru>
2023-09-01 18:37:10 +0600

    edit regularExpression.js add additional tests

--- _tests_/regularExpression.test.js
+++ _tests_/regularExpression.test.js
@@ -1,39 +1,54 @@
 import { regExp } from '../src/regularExpression';
 
 describe('Regular Expressions', () => {
-    it('matches valid date format', () => {
-      const regDate = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/;
-      const strdate = '23.05.1996';
-      expect(regDate.test(strdate)).toBe(true);
-    });
-  
-    it('does not match invalid date format', () => {
-      const regDate = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/;
-      const strdate = '32.05.1996'; // Invalid day
-      expect(regDate.test(strdate)).toBe(false);
-    });
-  
-    it('matches valid email format', () => {
-      const regmail = /[\da-z\.]+@[\da-z]+\.[a-z]+/;
-      const mail = 'john@gmail.com';
-      expect(regmail.test(mail)).toBe(true);
-    });
-  
-    it('does not match invalid email format', () => {
-      const regmail = /[\da-z\.]+@[\da-z]+\.[a-z]+/;
-      const mail = 'john@gmailcom'; // Invalid email format
-      expect(regmail.test(mail)).toBe(false);
-    });
-  
-    it('matches valid phone number format', () => {
-      const regPhone = /^\+\d{1,2}\s\d{3,4}\s\d{1,3}-\d{2}-\d{2}$/;
-      const strphone = '+7 999 999-99-99';
-      expect(regPhone.test(strphone)).toBe(true);
-    });
-  
-    it('does not match invalid phone number format', () => {
-      const regPhone = /^\+\d{1,2}\s\d{3,4}\s\d{1,3}-\d{2}-\d{2}$/;
-      const strphone = '123'; // Invalid phone format
-      expect(regPhone.test(strphone)).toBe(false);
-    });
+  jest.spyOn(window, "prompt").mockImplementation(() => "23.05.1996");
+
+  const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+
+  afterAll(() => {
+    window.prompt.mockRestore();
+    console.log.mockRestore();
   });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('matches valid date format', () => {
+    regExp();
+    expect(consoleLogSpy).toHaveBeenCalledWith('valid date');
+  });
+
+  it('does not match invalid date format', () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "32.05.1996");
+    expect(() => {
+      regExp();
+    }).toThrowError(Error("Invalid data"));
+  });
+
+  it('matches valid phone number format', () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "+7 999 999-99-99");
+    regExp();
+    expect(consoleLogSpy).toHaveBeenCalledWith('valid phone number');
+  });
+
+  it('should throw an error when phone number format does not match ', () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "+70 9999 999-99-99");
+    expect(() => {
+      regExp();
+    }).toThrowError(Error("Invalid data"));
+  });
+
+  it('matches valid email format', () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "john@gmail.com");
+    regExp();
+    expect(consoleLogSpy).toHaveBeenCalledWith('valid email');
+  });
+
+  it('does not match invalid email format', () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "john@gmailcom");
+    expect(() => {
+      regExp();
+    }).toThrowError(Error("Invalid data"));
+  });
+});
--- src/regularExpression.js
+++ src/regularExpression.js
@@ -1,13 +1,19 @@
 export function regExp() {
+    const str = prompt ('enter string');
+
     const regDate = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/;
-    const strdate = '23.05.1996';
-    console.log(regDate.test(strdate));
-
+    // const strdate = '23.05.1996';
     const regmail = /[\da-z\.]+@[\da-z]+\.[a-z]+/;
-    const mail = 'john@gmail.com';
-    console.log(regmail.test(mail));
-
-    const regPhone = /^\+\d{1,2}\s\d{3,4}\s\d{1,3}-\d{2}-\d{2}$/;
-    const strphone = '+7 999 999-99-99';
-    console.log(regPhone.test(strphone));
+    // const mail = 'john@gmail.com';
+        const regPhone = /^\+\d{1,2}\s\d{3}\s\d{2,3}-\d{2}-\d{2}$/;
+    // const strphone = '+7 999 999-99-99';
+    if (regDate.test(str)) {
+        console.log('valid date');
+    } else if (regmail.test(str)) {
+        console.log('valid email');
+    } else if(regPhone.test(str)) {
+        console.log('valid phone number');
+    } else {
+        throw new Error('Invalid data');
+    }
 }
