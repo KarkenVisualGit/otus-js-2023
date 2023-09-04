b810c79cc8dd8fc85e70bfd0245ffefb1a4c7475
Karken Turarov <karken_turarov@mail.ru>
2023-09-04 18:21:38 +0600

    edit triangle.js

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -3,7 +3,7 @@ import { checkTriangleSides } from "../src/triangle";
 test("Triangle with valid sides should log 'Triangle is rectangle'", () => {
   const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
 
-  checkTriangleSides(3, 4, 5);
+  checkTriangleSides(3, 5, 4);
 
   expect(consoleLogSpy).toHaveBeenCalledWith("Triangle is rectangle");
 
@@ -12,6 +12,6 @@ test("Triangle with valid sides should log 'Triangle is rectangle'", () => {
 
 test("Triangle with invalid sides should throw an error", () => {
   expect(() => {
-    checkTriangleSides(3, 4, 6);
+    checkTriangleSides(6, 4, 3);
   }).toThrowError("Triangle is not rectangular");
 });
