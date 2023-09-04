da9849afc037406e1cf960a4e44fca8cca3ad98e
Karken Turarov <karken_turarov@mail.ru>
2023-09-04 12:20:05 +0600

    edit arrays.js arrays.test.js

--- _tests_/arrays.test.js
+++ _tests_/arrays.test.js
@@ -1,17 +1,18 @@
-const arrayElements = require("../src/arrays");
+import {orderArray, getSum, doubleArray} from "../src/arrays";
 
 describe("Test arrayElements for sum, min and max elements", () => {
   it("Must console log sum of elements, min and max elements", () => {
     const nums = [5, 1, 3, 3, 7, 2, 5, 2, 2, 7];
     const logSpy = jest.spyOn(global.console, "log");
-    arrayElements(nums);
+    orderArray(nums);
+    getSum(nums);
 
     expect(logSpy).toHaveBeenCalled();
     expect(logSpy).toHaveBeenCalledTimes(3);
 
-    expect(logSpy).toHaveBeenCalledWith(37);
     expect(logSpy).toHaveBeenCalledWith(7);
-    expect(logSpy.mock.calls).toContainEqual([1]);
+    expect(logSpy).toHaveBeenCalledWith(1);
+    expect(logSpy.mock.calls).toContainEqual([37]);
 
     logSpy.mockRestore();
   });
@@ -19,6 +20,6 @@ describe("Test arrayElements for sum, min and max elements", () => {
 describe("Test arrayElements for returning doubled array", () => {
   it("Must return doubled array", () => {
     const nums = [5, 1, 3, 3, 7, 2, 5, 2, 2, 7];
-    expect(arrayElements(nums)).toEqual([10, 2, 6, 6, 14, 4, 10, 4, 4, 14]);
+    expect(doubleArray(nums)).toEqual([10, 2, 6, 6, 14, 4, 10, 4, 4, 14]);
   });
 });
--- src/arrays.js
+++ src/arrays.js
@@ -1,21 +1,27 @@
 // const nums = [5, 1, 3, 3, 7, 2, 5, 2, 2, 7];
 let sum = 0;
 let orderedArray = [];
-// for (let i = 0; i < 10; i++) {
-//     nums.push(Math.ceil(Math.random(0.5) * 7));
-// }
-function arrayElements(nums) {
-  nums.forEach((element) => {
-    sum += element;
-  });
-  console.log(sum);
-  const array = nums.map((x) => x * 2);
+
+export function orderArray(nums) {
   orderedArray = nums.sort((a, b) => a - b);
   let min = orderedArray[0];
   let max = orderedArray[9];
   console.log(max);
   console.log(min);
+}
+
+
+export function getSum(nums) {
+  nums.forEach((element) => {
+    sum += element;
+  });
+  console.log(sum);
+  return sum;
+}
+
+
+export function doubleArray(nums) {
+  const array = nums.map((x) => x * 2);
   return array;
 }
 
-module.exports = arrayElements;
